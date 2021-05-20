a45656db80fe3875a9a78986faf7927e3877bb55
MaheshSampath123 <83757151+MaheshSampath123@users.noreply.github.com>
2021-05-20 12:40:18 +0530

    Properties

---
+++ Properties/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Properties
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u = new User();
+            u.Name = "mahesh sampath";
+            u.Location = "Hyderabad";
+            Console.WriteLine("Name: " + u.Name);
+            Console.WriteLine("Location: " + u.Location);
+            Console.WriteLine("\nPress Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}
